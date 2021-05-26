0c6d44a3fd043895486e35876bbfffa0512e51f2
Justin O'Dwyer <justinodwyer@college.harvard.edu>
2021-05-26 10:29:48 -0400

    added a new training.py

--- jumping_task/boxjump/training.py
+++ jumping_task/boxjump/training.py
@@ -1,42 +1,47 @@
-import gym
+# import the class JumpTaskEnv defined in jumping_task.py
+import numpy as np
+import pygame
+import time
 import sys
 
-sys.path.append('/home/simon/Downloads/stable-baselines3')
-sys.path.append('/home/simon/Downloads/jumping-task/gym-jumping-task')
-print(sys.path)
-from stable_baselines3 import DQN, TD3, A2C, PPO
-from gym_jumping_task.envs import JumpTaskEnv
-from stable_baselines3.common.env_checker import *
-import numpy as np
-from stable_baselines3.ddpg.policies import MlpPolicy
+sys.path.append(".")
 
+from jumping_task import JumpTaskEnv
 
+# initialize the environment
+env = JumpTaskEnv(scr_w=60, scr_h=60)
 
-env = gym.make('jumping-task-v0', rendering=True) #
+def test():
+  env = JumpTaskEnv(scr_w=60, scr_h=60, obstacle_position=0, rendering=True, slow_motion=True, finish_jump=False)
+  env.render()
+  score = 0
+  while not env.done:
+    action = None
+    if env.jumping[0] and env.finish_jump:
+      action = 3
+    else:
+      events = pygame.event.get()
+      for event in events:
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_RIGHT:
+            action = 0
+          elif event.key == pygame.K_UP:
+            action = 1
+          elif event.key == pygame.K_e:
+            env.exit()
+          else:
+            action = 'unknown'
+    if action is None:
+      continue
+    elif action == 'unknown':
+      print('We did not recognize that action. Please use the arrows to move the agent or the \'e\' key to exit.')
+      continue
+    _, reward, term, _ = env.step(action)
+    env.render()
+    score += reward
+    print('Agent position: {:2d} | Reward: {:2d} | Terminal: {}'.format(env.agent_pos_x, reward, term))
+  print('---------------')
+  print('Final score: {:2d}'.format(int(score)))
+  print('---------------')
 
-obs = env.reset()
-obs, r, done, info = env.step(0)
-print(type(r))
-print("obs:", obs.shape)
-print(env.observation_space.shape)
-print("length", len(env.observation_space.shape))
-
-check_env(env)
-
-
-
-
-
-training_steps = 2000000
-
-learning_rate = 0.0001
-# Initialize the model0
-
-
-model = DQN("CnnPolicy", env, learning_rate = learning_rate, exploration_final_eps = 0.05, verbose=1) #
-# Train the model
-model.learn(training_steps, eval_env=env, eval_freq= 10000, n_eval_episodes=10, eval_log_path = "log")
-
-model.save("./jumping_model")
-
-env.close()
+test()
